3ad4eedfc97f697606ed464f05d2e7b7becc0eb6
carljhonsonk <144616526+carljhonsonk@users.noreply.github.com>
2026-08-13 22:20:29 +0530

    Add files via upload

---
+++ Arrays/Second.java
@@ -0,0 +1,17 @@
+public class Second{
+    public static void main(String[] args) {
+    int [] arr = {1,2,3,4,5,6,7,8};
+    int largest = arr[0];
+    int secondlargest = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            secondlargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondlargest){
+         secondlargest = arr[i];   
+        }
+    }    
+    }
+    
+}
